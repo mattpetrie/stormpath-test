0294ea072e1e6b6a1c9f956e7ce7d74d6cbb2e3b
Matt Petrie <matthew.petrie@gmail.com>
2015-06-23 23:13:43 -0700

    use ENV variable to set port

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ var app = require('./server');
 
 var PORT = process.env.PORT || 8000;
 
-app.listen(8000);
+// Initialize the server
+app.listen(PORT, function() {
+  console.log('Server listening on port', PORT);
+});
